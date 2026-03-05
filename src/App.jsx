f0b22f555c4a753e9a7b434ca2ac3ead023be6d3
Samie Syed <samsonknight2k@hotmail.com>
2026-03-05 15:47:55 +0000

    Add decrease feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
+//importing react state. React component, remembers values between renders
 import { useState } from "react";
 
 export default function App() {
 
+
+// this means count → stored value
+//setCount → function to change it
+//0 → starting value
+//setCount built in react function to rerender component automatically
+//state changed
+//rerender component
+//update UI
+
   const [count, setCount] = useState(0)
 
+
+ //this is the function which increases the count
   function increase() {
     setCount(count + 1)
+  }
+
+  //this is the function which decreases the count
+  function decrease() {
+    setCount(count - 1)
   }
 
   return (
@@ -14,8 +31,12 @@ export default function App() {
 
       <p>Count: {count}</p>
 
+{/*call function when button is clicked */}
       <button onClick={increase}>
         Increase
+      </button>
+      <button onClick={decrease}>
+        Decrease
       </button>
     </div>
   )
