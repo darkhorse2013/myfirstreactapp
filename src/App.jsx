f0ab75880cb0f7fd8f4a4c5d7c1a49e796099582
Samie Syed <samsonknight2k@hotmail.com>
2026-03-05 13:35:38 +0000

    simple code in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,8 @@ export default function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div style={{ fontFamily: "Arial, sans-serif", padding: 24 }}>
-      <h1>My First React App</h1>
-      <p>This button proves React is working:</p>
-
+    <div style={{padding:40}}>
+      <h1>React Counter</h1>
       <button onClick={() => setCount(count + 1)}>
         Clicked {count} times
       </button>
