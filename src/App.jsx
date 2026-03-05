e136512220fe9e47e882ab3ac92f0718153c65fc
Samie Syed <samsonknight2k@hotmail.com>
2026-03-05 13:44:09 +0000

    Add counter feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import { useState } from "react";
 
 export default function App() {
-  const [count, setCount] = useState(0);
+
+  const [count, setCount] = useState(0)
+
+  function increase() {
+    setCount(count + 1)
+  }
 
   return (
     <div style={{padding:40}}>
       <h1>React Counter</h1>
-      <button onClick={() => setCount(count + 1)}>
-        Clicked {count} times
+
+      <p>Count: {count}</p>
+
+      <button onClick={increase}>
+        Increase
       </button>
     </div>
-  );
+  )
 }
